fd332f1d858af72b0adf5606dafd454ed6d60e73
mohsensami <mohsensami18727@gmail.com>
2024-07-01 19:59:29 +0330

    Jobs and JobsRow component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero';
+import Jobs from './components/Jobs';
 
 export default async function Home() {
     return (
         <>
             <Hero />
+            <Jobs />
         </>
     );
 }
