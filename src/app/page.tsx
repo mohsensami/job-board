5c99a06263e294a6959e9354c23729c33ff2fb8b
mohsensami <mohsensami18727@gmail.com>
2024-07-01 19:26:01 +0330

    Hero component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-    return <div>test</div>;
+import Hero from './components/Hero';
+
+export default async function Home() {
+    return (
+        <>
+            <Hero />
+        </>
+    );
 }
